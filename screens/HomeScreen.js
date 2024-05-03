0b2bc5c806cf7ce46531450ddb6c140caebc9b36
Soufiane Joumal <joumalsoufian17@gmail.com>
2024-05-03 20:11:01 +0100

    search bar added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,46 @@
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar';
+import {theme} from '../theme'
+
+import {MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+
+export default function HomeScreen(){
+    const [showSearch, toggleSeach] =useState(false);
+    const [locations, setLocations] = useState([1,2,3])
+    return(
+        <View className='flex-1 relative'>
+            <StatusBar style="Light " />
+            <Image blurRadius={70}source={require('../assets/images/bg.png')}
+               className="absolute h-full w-full"
+               />
+              <SafeAreaView className="flex flex-1">
+                {/* search section */}
+                <View style={{marginTop: '11%', height:'7%'}} className="mx-4 relative z-50">
+                    <View className="flex-row justify-end items-center rounded-full"
+                    style={{backgroundColor:showSearch? theme.bgWhite(0.2):'transparent'}}>
+                        {
+                            showSearch? (
+                        <TextInput
+                        placeholder='Search city'
+                        placeholderTextColor={'lightgray'}
+                        className="pl-6 h-10 flex-1 text-base text-white"
+                        /> 
+                            ):null
+                        }
+                        
+                        <TouchableOpacity
+
+                            onPress={()=>toggleSeach(!showSearch)}
+                            style={{backgroundColor: theme.bgWhite(0.3)}}
+                            className="rounded-full p-3 m-1"
+                        >
+                            <MagnifyingGlassIcon size="25" color="white" />
+                        </TouchableOpacity>
+                    </View>
+                  </View>
+                 </SafeAreaView>
+        </View>
+
+    )
+}
