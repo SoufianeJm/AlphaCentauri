3a67d039e995a211cc04fd87a65c45e6e7e73e4f
Soufiane Joumal <joumalsoufian17@gmail.com>
2024-05-04 08:35:15 +0100

    code format

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,117 +1,140 @@
-import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar';
-import {theme} from '../theme'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { theme } from "../theme";
 
-import {MagnifyingGlassIcon} from 'react-native-heroicons/outline'
-import {MapPinIcon} from 'react-native-heroicons/solid'
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { MapPinIcon } from "react-native-heroicons/solid";
 
-export default function HomeScreen(){
-    const [showSearch, toggleSeach] =useState(false);
-    const [locations, setLocations] = useState([1,2,3]);
+export default function HomeScreen() {
+  const [showSearch, toggleSeach] = useState(false);
+  const [locations, setLocations] = useState([1, 2, 3]);
 
-    const handlelocation = (loc) => {
-        console.log('location', loc);
-    }
-    return(
-        <View className='flex-1 relative'>
-            <StatusBar style="Light " />
-            <Image blurRadius={70}source={require('../assets/images/bg.png')}
-               className="absolute h-full w-full"
-               />
-              <SafeAreaView className="flex flex-1">
-                {/* search section */}
-                <View style={{marginTop: '11%', height:'7%'}} className="mx-4 relative z-50">
-                    <View className="flex-row justify-end items-center rounded-full"
-                    style={{backgroundColor:showSearch? theme.bgWhite(0.2):'transparent'}}>
-                        {
-                            showSearch? (
-                                <TextInput
-                                placeholder='Search city'
-                                placeholderTextColor={'lightgray'}
-                                className="pl-6 h-10 flex-1 text-base text-white"
-                                /> 
-                            ):null
-                        }
-                        
-                        <TouchableOpacity
+  const handlelocation = (loc) => {
+    console.log("location", loc);
+  };
+  return (
+    <View className="flex-1 relative">
+      <StatusBar style="Light " />
+      <Image
+        blurRadius={70}
+        source={require("../assets/images/bg.png")}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        {/* search section */}
+        <View
+          style={{ marginTop: "11%", height: "7%" }}
+          className="mx-4 relative z-50"
+        >
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{
+              backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent",
+            }}
+          >
+            {showSearch ? (
+              <TextInput
+                placeholder="Search city"
+                placeholderTextColor={"lightgray"}
+                className="pl-6 h-10 flex-1 text-base text-white"
+              />
+            ) : null}
 
-                            onPress={()=>toggleSeach(!showSearch)}
-                            style={{backgroundColor: theme.bgWhite(0.3)}}
-                            className="rounded-full p-3 m-1"
-                        >
-                            <MagnifyingGlassIcon size="25" color="white" />
-                        </TouchableOpacity>
-                    </View>
-                    {
-                        locations.length>0 && showSearch? (
-                            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
-                                {
-                                locations.map((loc, index)=>{
-                                    let showBorder = index+1 != locations.length;
-                                    let borderClass = showBorder? ' border-b-2 border-b-gray-400': '';
-                                    return(
-                                        <TouchableOpacity
-                                            onPress={()=> handlelocation(loc)}
-                                            key={index}
-                                            className={"flex-row items-center border-0 p-3 px-4"+borderClass}
-                                        >
-                                            <MapPinIcon size="20" color="gray" />
-                                            <Text className="text-black text-lg ml-2">London, United Kingdom</Text>
-                                        </TouchableOpacity>
-                                    )
-                                })
-                            }
-                            </View>
-                        ):null
+            <TouchableOpacity
+              onPress={() => toggleSeach(!showSearch)}
+              style={{ backgroundColor: theme.bgWhite(0.3) }}
+              className="rounded-full p-3 m-1"
+            >
+              <MagnifyingGlassIcon size="25" color="white" />
+            </TouchableOpacity>
+          </View>
+          {locations.length > 0 && showSearch ? (
+            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+              {locations.map((loc, index) => {
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? " border-b-2 border-b-gray-400"
+                  : "";
+                return (
+                  <TouchableOpacity
+                    onPress={() => handlelocation(loc)}
+                    key={index}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4" + borderClass
                     }
-                  </View>
-                    {/* forecast section*/}
-                    <View className="mx-4 flex justify-around flex-1 mb-2">
-                        {/* location */}
-                        <Text className="text-white text-center text-2xl font-bold">
-                            London,
-                            <Text className="text-lg font-semibold text-gray-300"> 
-                                United Kingdom
-                            </Text>
-                        </Text>
-                            {/* weather image */}
-                            <View className="flex-row justify-center">
-                                <Image
-                                    source={require('../assets/images/partlycloudy.png')}
-                                    className="w-52 h-52"
-                                />
-                            </View>
-                            {/* temperature */}
-                            <View className="space-y-2">
-                                <Text className="text-center font-bold text-white text-6xl ml-5">
-                                    23&#176;
-                                </Text>
-                                <Text className="text-center text-white text-xl tracking-widest">
-                                    Partly Cloudy
-                                </Text>
-                            </View>
-                            <View className="flex-row justify-between mx-4">
-
-                                <View className="flex-row space-x-2 items-center">
-                                    <Image source={require('../assets/icons/wind.png')} className="h-6 w-6" />
-                                    <Text className="text-white font-semibold text-base">22km</Text>
-                                </View>
-
-                                <View className="flex-row space-x-2 items-center">
-                                    <Image source={require('../assets/icons/drop.png')} className="h-6 w-6" />
-                                    <Text className="text-white font-semibold text-base">23%</Text>
-                                </View>
-
-                                <View className="flex-row space-x-2 items-center">
-                                    <Image source={require('../assets/icons/sun.png')} className="h-6 w-6" />
-                                    <Text className="text-white font-semibold text-base">6:05 AM</Text>
-                                </View>
-
-                            </View>
-                    </View>
-                 </SafeAreaView>
+                  >
+                    <MapPinIcon size="20" color="gray" />
+                    <Text className="text-black text-lg ml-2">
+                      London, United Kingdom
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          ) : null}
         </View>
+        {/* forecast section*/}
+        <View className="mx-4 flex justify-around flex-1 mb-2">
+          {/* location */}
+          <Text className="text-white text-center text-2xl font-bold">
+            London,
+            <Text className="text-lg font-semibold text-gray-300">
+              United Kingdom
+            </Text>
+          </Text>
+          {/* weather image */}
+          <View className="flex-row justify-center">
+            <Image
+              source={require("../assets/images/partlycloudy.png")}
+              className="w-52 h-52"
+            />
+          </View>
+          {/* temperature */}
+          <View className="space-y-2">
+            <Text className="text-center font-bold text-white text-6xl ml-5">
+              23&#176;
+            </Text>
+            <Text className="text-center text-white text-xl tracking-widest">
+              Partly Cloudy
+            </Text>
+          </View>
+          <View className="flex-row justify-between mx-4">
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/wind.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22km</Text>
+            </View>
 
-    )
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/drop.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">23%</Text>
+            </View>
+
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../assets/icons/sun.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
 }
